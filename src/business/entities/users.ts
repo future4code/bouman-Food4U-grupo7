77c40907db7e25ea189055ef48803c6593cd1ad8
Settlers <ssaquo@gmail.com>
2020-03-16 14:36:27 -0300

    subindo esqueleto pronto, com todas pastas e bibliotecas

---
+++ src/business/entities/users.ts
@@ -0,0 +1,14 @@
+export class User {
+   constructor(
+      private email: string,
+      private password: string,
+   ) {}
+
+   public getEmail(): string {
+      return this.email;
+   }
+
+   public getPassword(): string {
+      return this.password
+   }
+}
--- src/presentation/index.ts
+++ src/presentation/index.ts
@@ -3,4 +3,6 @@ import express, { Request, Response } from "express";
 const app = express();
 app.use(express.json());
 
+app.post('/')
+
 export default app;
